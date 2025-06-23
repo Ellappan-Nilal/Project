74bf77fb080b1832b5d0a6f72b827cc33d3f41ca
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-23 22:25:36 +0530

    file change

--- Elevate/Calculator.java
+++ Elevate/Calculator.java
@@ -20,7 +20,8 @@ public class Calculator {
             System.out.println("1. Addition (+)");
             System.out.println("2. Subtraction (-)");
             System.out.println("3. Multiplication (*)");
-            
+            System.out.println("4. Division (/)");
+            System.out.println("5. Exit");
            
             int choice;
             try {
