7c1143fa611823aefb4518c2f1e093511d8e8c73
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-26 19:33:28 +0530

    files are added

---
+++ Task3_Library/Book.java
@@ -0,0 +1,43 @@
+package Task3_Library;
+class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isIssued; 
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isIssued = false; 
+    }
+
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+
+    public void setIssued(boolean issued) {
+        isIssued = issued;
+    }
+
+    public void displayBook() {
+        System.out.println("Title: " + title +
+                           ", Author: " + author +
+                           ", ISBN: " + isbn +
+                           ", Status: " + (isIssued ? "Issued" : "Available"));
+    }
+}
