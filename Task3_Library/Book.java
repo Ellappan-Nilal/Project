a49f2708fa25a3d07f029f48348de53b2aa3d1e6
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-28 19:22:02 +0530

    Task 1 is added

--- Task3_Library/Book.java
+++ Task3_Library/Book.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package Task3_Library;
 class Book {
     private String title;
@@ -40,4 +41,48 @@ class Book {
                            ", ISBN: " + isbn +
                            ", Status: " + (isIssued ? "Issued" : "Available"));
     }
+=======
+package Task3_Library;
+class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isIssued; 
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isIssued = false; 
+    }
+
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+
+    public void setIssued(boolean issued) {
+        isIssued = issued;
+    }
+
+    public void displayBook() {
+        System.out.println("Title: " + title +
+                           ", Author: " + author +
+                           ", ISBN: " + isbn +
+                           ", Status: " + (isIssued ? "Issued" : "Available"));
+    }
+>>>>>>> 7c1143f (files are added)
 }
